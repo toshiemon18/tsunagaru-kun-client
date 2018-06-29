0aae5a08d7d686321f2c853e16cac0ebd4b99b20
Kenkichi Hattori <service@ahaufugyaha.com>
2018-06-29 17:26:23 +0900

    First Commit

---
+++ tsunagaru-kun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tsunagaru-kun
+//
+//  Created by pcwin7 on 2018/06/29.
+//  Copyright © 2018年 softech-teama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
