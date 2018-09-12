313fa5c41ab30a411816c296fa5df6f1bc049449
corazon_tomori <tsubaki.corazon@gmail.com>
2018-09-12 13:38:00 +0900

    ログイン状態によってのストーリーボードの処理を追加

--- tsunagaru-kun/AppDelegate.swift
+++ tsunagaru-kun/AppDelegate.swift
@@ -15,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let login = UserDefaults.standard.bool(forKey: "login")
+
+        var next: UIViewController!
+        
+        if login {
+            let storyboard = UIStoryboard(name: "Devicelist", bundle: nil)
+            next = storyboard.instantiateInitialViewController()
+        } else {
+            let storyboard = UIStoryboard(name: "Login", bundle: nil)
+            next = storyboard.instantiateInitialViewController()
+        }
+        self.window?.rootViewController = next
         // Override point for customization after application launch.
         return true
     }
