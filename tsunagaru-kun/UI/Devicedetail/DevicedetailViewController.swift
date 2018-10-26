1a2551287d819e1fdc4bba5d5ee9e20885102efd
Toshiaki Seino <st12318@gmail.com>
2018-10-27 03:02:22 +0900

    HTTPクライアントを実装（WIP)

--- tsunagaru-kun/UI/Devicedetail/DevicedetailViewController.swift
+++ tsunagaru-kun/UI/Devicedetail/DevicedetailViewController.swift
@@ -14,17 +14,18 @@ import SwiftyJSON
 class DevicedetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var detailTable: UITableView!
     
-    let evaluationVals = ["電圧[V]", "電流量[A]", "有効電力[W]",
+    let evaluationStrs = ["電圧[V]", "電流量[A]", "有効電力[W]",
                           "換算電力量[W]", "電気料金[円/月]", "CO2排出量[kg]",
                           "待機時間[h]", "待機時の消費電力[W]"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return evaluationVals.count
+        return evaluationStrs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel!.text = evaluationVals[indexPath.row]
+        cell.textLabel!.text = evaluationStrs[indexPath.row]
+        cell.detailTextLabel?.text = "10"
         return cell
     }
     
@@ -38,6 +39,24 @@ class DevicedetailViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func fetchDeviceDetails() {
+        var headers = ["Content-Type": "application/json"]
+        let userDefaults = UserDefaults.standard
+        headers["client"] = userDefaults.string(forKey: "client")
+        headers["access-token"] = userDefaults.string(forKey: "access-token")
+        headers["uid"] = userDefaults.string(forKey: "uid")
         
+        Alamofire.request("\(BASE_URL)/v1/devices/1", method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+            guard let object = response.result.value else {
+                return
+            }
+            let json = JSON(object)
+            
+            if json["errors"] == JSON.null {
+                
+            }
+            else {
+                // TODO: Implement!!!!
+            }
+        }
     }
 }
