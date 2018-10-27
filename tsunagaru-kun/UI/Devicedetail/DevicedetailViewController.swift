264a2d1615a2a65377527b92232cffe62fb0698e
Toshiaki Seino <st12318@gmail.com>
2018-10-27 16:41:28 +0900

    返り値の型を指定してもうまくできないのでメンバー変数を作ってそこにリストを放り込むことにした

--- tsunagaru-kun/UI/Devicedetail/DevicedetailViewController.swift
+++ tsunagaru-kun/UI/Devicedetail/DevicedetailViewController.swift
@@ -17,6 +17,8 @@ class DevicedetailViewController: UIViewController, UITableViewDelegate, UITable
     let evaluationStrs = ["電圧[V]", "電流量[A]", "有効電力[W]",
                           "換算電力量[W]", "電気料金[円/月]", "CO2排出量[kg]",
                           "待機時間[h]", "待機時の消費電力[W]"]
+    var ecList: Array<Double> = []
+    var wattList: Array<Double> = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return evaluationStrs.count
@@ -32,13 +34,14 @@ class DevicedetailViewController: UIViewController, UITableViewDelegate, UITable
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchDeviceDetail()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func fetchDeviceDetails() {
+    func fetchDeviceDetail() {
         var headers = ["Content-Type": "application/json"]
         let userDefaults = UserDefaults.standard
         headers["client"] = userDefaults.string(forKey: "client")
@@ -52,10 +55,9 @@ class DevicedetailViewController: UIViewController, UITableViewDelegate, UITable
             let json = JSON(object)
             
             if json["errors"] == JSON.null {
-                
-            }
-            else {
-                // TODO: Implement!!!!
+                let metricsList = json["half_year_metrics"].arrayValue
+                self.ecList = metricsList.map { $0["electric_current"].double! }
+                self.wattList = metricsList.map { $0["watt"].double! }
             }
         }
     }
